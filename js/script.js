93ea499c96d52e91c0b1e780b1f7a09f4dc2850a
Luiz André <luizandrenf@gmail.com>
2024-04-04 20:57:43 -0400

    acertado caminho da api

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ const funcionamento = new Funcionamento("[data-semana]", "aberto");
 funcionamento.init();
 
 
-fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchAnimais("../animaisapi.json", ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
 
 
